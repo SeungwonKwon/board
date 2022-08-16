9cb5c78e6bd4aabd369b0a59d73aed496c1fa497
SeungwonKwon <seungwon03@khu.ac.kr>
2022-08-16 22:40:20 +0900

    커밋

--- src/main/java/com/study/board/controller/UserController.java
+++ src/main/java/com/study/board/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.study.board.controller;
 
+import com.study.board.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,10 +13,18 @@ import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util.println
 @Controller
 public class UserController {
 
-    @GetMapping("/board/login")
-    @ResponseBody
-    public String userLogin(){
+    @Autowired
+    private UserRepository userRepository;
 
-        return "123123";
+    @GetMapping("/board/signin")
+    public String userSignin(){
+
+        return "usersignin";
+    }
+
+    @GetMapping("/board/signup")
+    public String userSignup(){
+
+        return "usersignup";
     }
 }
